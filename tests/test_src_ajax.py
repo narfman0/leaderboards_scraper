6d81437cf0bc9cd6778280c4ed2711d484fc5419
Jon Robison <narfman0@gmail.com>
2021-08-08 12:18:04 -0400

    Remove ajax for now

--- tests/test_src_ajax.py
+++
@@ -1,9 +0,0 @@
-import json
-import unittest
-from leaderboards_scraper import src_ajax
-
-
-class TestSrcAjax(unittest.TestCase):
-    def test_store_category_runs(self):
-        with open("tests/fixtures/smb3WarplessAjax1.html") as file:
-            src_ajax.store_category_runs("abc123", file.read())
