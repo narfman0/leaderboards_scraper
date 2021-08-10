c716ca6f7b53b1d0e23bbc3aeaf4a4d43563d65d
Jon Robison <narfman0@gmail.com>
2021-08-09 21:13:18 -0400

    Add some low hanging fruit :)

--- leaderboards_scraper/md.py
+++ leaderboards_scraper/md.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 def generate_category_leaderboard(category_id, runs, player_id_to_players):
     return "\n".join(
         generate_categories_header()
@@ -11,7 +14,11 @@ def generate_categories_rows(category_id, runs, player_id_to_players):
             player_id_to_players[player_id].name for player_id in run.player_ids
         ]
         player_names_agg = ",".join(player_names)
-        yield f"| {i} | {player_names_agg} | {run.time}s | {run.date} | {run.video_url} |"
+
+        m, s = divmod(run.time, 60)
+        h, m = divmod(m, 60)
+        run_time = "%d:%02d:%02d" % (h, m, s)
+        yield f"| {i} | {player_names_agg} | {run_time} | {run.date} | [link]({run.video_url}) |"
 
 
 def generate_categories_header():
